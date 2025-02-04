1baca30c51da9bfbf7e2a8ded6b5dd1ae49cfb41
Shaunak Fadnis <fadnisshaunak@gmail.com>
2025-02-03 18:47:29 -0600

    added wpa into graph creation, now need to add ranking method to scale nodes in cytoscape

--- graph_objects/gameGraphModels.py
+++ graph_objects/gameGraphModels.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass, field
 import nba_api.stats.endpoints as nba_stats
 import nba_api.live.nba.endpoints as nba_live
 from nba_api.stats.static import players, teams
-from graph_objects import utils
-# import utils
+# from graph_objects import utils
+import utils
 from typing import Dict
 import polars as pl
 import re
 import time
+import joblib
+import numpy as np
 
 
 TEAM_VIEW_TYPE = "TEAM"
@@ -115,6 +117,8 @@ class playerNode:
             "BLK" : self.BLK,
             "TO" : self.TO,
             "PF" : self.PF,
+            "WPA_NET" : self.wpa_net,
+            "WPA_ABS" : self.wpa_absolute
         }
         
         return stats_dict
@@ -139,6 +143,8 @@ class gameGraphBase:
         
         self.graph_nodes = {}
         
+        self.wpa_model = joblib.load("../wpa_model/wpa_model.pkl")
+        
         
     '''
 
@@ -161,18 +167,25 @@ class gameGraphBase:
             if (action := event[PLAY_ACTION_INDEX]) == -1:
                 continue
             
-            p1_offense = False
+            home_event = False
             
             # Inference columns for win prob: [PERIOD, PCTIMESTRING, scoreHome, scoreAway, PLAY_ACTION, HOME_AWAY_BOOL]
             win_prob_row = [event[PERIOD_INDEX], event[PCTIMESTRING_INDEX], event[SCORE_HOME_INDEX], event[SCORE_AWAY_INDEX], event[PLAY_ACTION_INDEX]]
             if event[P1_TEAM_ID_INDEX] == self.home_team_id:
                 win_prob_row.append(1)
-                p1_offense = True
+                home_event = True
             else:
                 win_prob_row.append(0)
-                
-            play_win_prob = 0 # call wpa model 
-            wpa = play_win_prob - home_win_prob # get the change in win probability
+             
+            # predict_proba expects a 2d array, might as well convert to np here rather than sklearn call doing it
+            win_prob_row_reshape = np.array(win_prob_row).reshape(1, -1)
+            
+            # predict home win probability after event (class of 1 is home_win)
+            play_win_prob = self.wpa_model.predict_proba(win_prob_row_reshape)[0][1]
+            
+            # get change in win probability as win probability added (wpa)
+            wpa = play_win_prob - home_win_prob
+            wpa_abs = abs(wpa)
             
             # STL
             if action == 1:
@@ -198,8 +211,15 @@ class gameGraphBase:
                 from_node.STL += 1
                 to_node.TO += 1
                 
-                from_node.updateWPA(wp_change=wpa)
-                to_node.updateWPA(wp_change=wpa)
+                from_node.wpa_absolute += wpa_abs
+                to_node.wpa_absolute += wpa_abs
+                
+                if home_event:
+                    from_node.wpa_net -= wpa
+                    to_node.wpa_net += wpa
+                else:
+                    from_node.wpa_net += wpa
+                    to_node.wpa_net -= wpa
                 
                 
                 # update game_edge to reflect event
@@ -229,8 +249,16 @@ class gameGraphBase:
                 # update involved player nodes
                 from_node.BLK += 1
                 
-                from_node.updateWPA(wp_change=wpa)
-                to_node.updateWPA(wp_change=wpa)
+                # update player wpa
+                from_node.wpa_absolute += wpa_abs
+                to_node.wpa_absolute += wpa_abs
+                
+                if home_event:
+                    from_node.wpa_net -= wpa
+                    to_node.wpa_net += wpa
+                else:
+                    from_node.wpa_net += wpa
+                    to_node.wpa_net -= wpa
                 
                 # update game_edge to reflect event
                 game_edge = from_node.gameEdgeGetOrAdd(to_pid=event[P1_ID_INDEX], off_bool=False)
@@ -253,6 +281,12 @@ class gameGraphBase:
                 # if event[P1_ID_INDEX] == 1630162:   
                 #     print(f"PLAYER: {scorer_data.get("full_name")}, DESC: {event[DESCRIPTION_INDEX]}, CURR_PTS: {scorer_node.PTS}")
                 
+                scorer_node.wpa_absolute += wpa_abs
+                
+                if home_event:
+                    scorer_node.wpa_net += wpa
+                else:
+                    scorer_node.wpa_net -= wpa
                 
                 if (P2_DATA := players.find_player_by_id(event[P2_ID_INDEX])) and (P2_DATA.get("is_active") == True):
                     assister_node = self.playerNodeGetOrAdd(
@@ -264,9 +298,18 @@ class gameGraphBase:
                     # update assister's stats
                     # TODO: UPDATE THE ACTION_STAT TO BE ASSIST HERE
                     assister_node.AST += 1
+                    assister_node.wpa_absolute += wpa_abs
+                    
+                    if home_event:
+                        assister_node.wpa_net += wpa
+                    else:
+                        assister_node.wpa_net -= wpa
                     
                     game_edge = assister_node.gameEdgeGetOrAdd(to_pid=event[P1_ID_INDEX], off_bool=True)
                     game_edge.updateStatsEdge(action_stat=action, three_made=made_three_bool, wp_change=wpa)
+                    
+                
+                    
             
             # MISS
             elif action == 4:
@@ -276,7 +319,11 @@ class gameGraphBase:
                     player_name=player_data.get("full_name"),
                     player_team_id=event[P1_TEAM_ID_INDEX]
                 )
-                player_node.updateWPA(wp_change=wpa)
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
             
             # TURNOVER
             elif action == 5:
@@ -294,7 +341,11 @@ class gameGraphBase:
                 else:
                     player_node.TO += 1
                 
-                player_node.updateWPA(wp_change=wpa)
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
             
             # FOUL
             elif action == 6:
@@ -320,8 +371,16 @@ class gameGraphBase:
                 from_node.PF += 1
                 to_node.F_DRAWN += 1
                 
-                from_node.updateWPA(wp_change=wpa)
-                to_node.updateWPA(wp_change=wpa)
+                # update player wpa
+                from_node.wpa_absolute += wpa_abs
+                to_node.wpa_absolute += wpa_abs
+                
+                if home_event:
+                    from_node.wpa_net -= wpa
+                    to_node.wpa_net += wpa
+                else:
+                    from_node.wpa_net += wpa
+                    to_node.wpa_net -= wpa
                 
                 # update game_edge to reflect event
                 game_edge = from_node.gameEdgeGetOrAdd(to_pid=event[P2_ID_INDEX], off_bool=False)
@@ -336,7 +395,12 @@ class gameGraphBase:
                     player_team_id=event[P1_TEAM_ID_INDEX]
                 )
                 player_node.PTS += 1
-                player_node.updateWPA(wp_change=wpa)
+                
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
                 
                 # if event[P1_ID_INDEX] == 1630162:   
                 #     print(f"PLAYER: {player_data.get("full_name")}, DESC: {event[DESCRIPTION_INDEX]}, CURR_PTS: {player_node.PTS}")
@@ -349,7 +413,12 @@ class gameGraphBase:
                     player_name=player_data.get("full_name"),
                     player_team_id=event[P1_TEAM_ID_INDEX]
                 )
-                player_node.updateWPA(wp_change=wpa)
+                
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
 
             # REB
             elif action == 9:
@@ -360,7 +429,12 @@ class gameGraphBase:
                     player_team_id=event[P1_TEAM_ID_INDEX]
                 )
                 player_node.REB += 1
-                player_node.updateWPA(wp_change=wpa)
+                
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
 
             # TECH_FOUL
             elif action == 10:
@@ -371,7 +445,12 @@ class gameGraphBase:
                     player_team_id=event[P1_TEAM_ID_INDEX]
                 )                
                 player_node.F_TECH += 1
-                player_node.updateWPA(wp_change=wpa)
+                
+                player_node.wpa_absolute += wpa_abs
+                if home_event:
+                    player_node.wpa_net += wpa
+                else:
+                    player_node.wpa_net -= wpa
 
         
     def playerNodeGetOrAdd(self, player_id, player_name, player_team_id):
